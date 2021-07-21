1ff1f7f2a24bb270c680405b9dceb8810cfa2321
Andrey Konnov <a.p.konnov@mail.ru>
2021-07-22 00:49:55 +0400

    Create task_2_4.py

---
+++ task_2_4.py
@@ -0,0 +1,4 @@
+list_title_name = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА',
+                   'токарь высшего разряда нИКОЛАй', 'директор аэлита']
+for str_title_name in list_title_name:
+    print(f'Привет, {str_title_name.split()[-1].title()}!')
