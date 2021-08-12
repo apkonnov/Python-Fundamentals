c6bf3758e37d2f3b3621a9532a541e95c1d3acbc
Andrey Konnov <a.p.konnov@mail.ru>
2021-08-12 19:40:00 +0400

    Create task_7_2.py

---
+++ task_7_2.py
@@ -0,0 +1,26 @@
+import yaml
+import os
+
+
+def make_dirs_files(root_folder, mk_dict):
+    for key, val in mk_dict.items():
+        if type(val) == list:
+            folder = os.path.join(root_folder, key)
+            os.mkdir(folder)
+            for name in val:
+                if type(name) == dict:
+                    make_dirs_files(folder, name)
+                else:
+                    with open(os.path.join(folder, name), 'w', encoding='utf-8') as f:
+                        pass
+
+
+try:
+    with open('config.yaml') as f:
+        conf_dict = yaml.safe_load(f)
+        conf = conf_dict.popitem()
+    os.mkdir(conf[0])
+    make_dirs_files(conf[0], conf[1])
+    print('Папка для проекта создана')
+except Exception as e:
+    print(f'Еrr: {e}')
